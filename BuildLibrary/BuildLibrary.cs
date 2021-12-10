c76b6617c0641cee9ab56235910b0f2afec13b82
John Tyndall <john@tyndall.io>
2021-12-10 11:09:23 -0500

    Remove unnecessary usings

--- BuildLibrary/BuildLibrary.cs
+++ BuildLibrary/BuildLibrary.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuildLibrary
+﻿namespace BuildLibrary
 {
-    internal class BuildLibrary
+    public class BuildLibrary
     {
+        public void DoPersonStuff()
+        {
+            Person p = new Person();
+
+            p.FirstName = "Bill";
+
+            p.LastName = "Jones";
+
+
+        }
     }
 }
